d8acdc3fa78e7313543947d5b1554684629577ab
McSon2 <maximesaltet@gmail.com>
2022-03-02 16:03:38 +0100

    correct

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-const {app, BrowserWindow} = require ('electron')
-const { dirname } = require('path')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const { dirname } = require("path");
+const path = require("path");
 
 //Window
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        minWidth: 1024,
-        minHeight: 640,
-        closable: true,
-        darkTheme: true,
-        frame: true,
-        icon: path.join(__dirname, '/ico.ico'),
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: false,
-            devTools: true,
-            preload: path.join(__dirname,"preload.js")
-        }
-    })
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    minWidth: 1024,
+    minHeight: 640,
+    closable: true,
+    darkTheme: true,
+    frame: true,
+    icon: path.join(__dirname, "/ico.ico"),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: false,
+      devTools: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-    win.loadFile ("index.html")
-    win.webContents.openDevTools()
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
 }
 
 // When Electron Ready
 app.whenReady().then(() => {
-    createWindow()
+  createWindow();
 
-    app.on('activate',() => {
-        if(BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
-})
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
     }
-})
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
