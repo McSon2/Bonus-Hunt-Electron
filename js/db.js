62df163b694d821caeaca99a648886229265fc19
Mcson <40039615+McSon2@users.noreply.github.com>
2022-03-12 16:43:38 +0100

    Update db.js

--- js/db.js
+++ js/db.js
@@ -2,7 +2,13 @@ const sqlite = require('better-sqlite3');
 const db = new sqlite("./Database/bh.db");
 
 exports.getNbonus = () => {
-    const stmt = db.prepare('SELECT count(*) as nbonus FROM hunt');
+    const stmt = db.prepare('SELECT count(*) as nbonus FROM bonus_hunt');
+    const res = stmt.all();
+    return res;
+}
+
+exports.getTotalCost = () => {
+    const stmt = db.prepare('SELECT SUM(start) as totalcost FROM bonus_hunt');
     const res = stmt.all();
     return res;
 }
