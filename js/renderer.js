dff2187751ede136814f616fc62b4f7d10395ce9
Mcson <40039615+McSon2@users.noreply.github.com>
2022-03-12 16:43:21 +0100

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', async () =>{
-    let names = window.api.getNames();
-    let divNames = document.getElementById("names");
-    let nameString = names.map((elem) => {
-        return elem.nbbonus;
+    let nbonus = window.api.getNbonus();
+    let divNames = document.getElementById("nbonus");
+    let nameString = nbonus.map((elem) => {
+        return elem.nbonus;
     });
-    
+
     divNames.innerHTML = nameString;
+})
+
+document.addEventListener('DOMContentLoaded', async () =>{
+    let TotalCost = window.api.getTotalCost();
+    let divNames = document.getElementById("getTotalCost");
+    let nameString = TotalCost.map((elem) => {
+        return elem.totalcost;
+    });
+
+    divNames.innerHTML = "$" + nameString;
 })
