650402decc9fcdd13cbffb90d3d7d950ddec43c2
McSon2 <maximesaltet@gmail.com>
2022-03-18 23:18:12 +0100

    table

--- js/db.js
+++ js/db.js
@@ -43,7 +43,7 @@ exports.ProfitLoss = () => {
 }
 
 exports.bonushunt = () => {
-    const stmt = db.prepare('SELECT title, start,date,(Select count(*) from hunt where hunt.id_bonushunt = bonus_hunt.id) as nbbonus, ((SELECT SUM(payout) FROM hunt where hunt.id_bonushunt = bonus_hunt.id)-(SELECT start FROM bonus_hunt where bonus_hunt.id = hunt.id_bonushunt)) as profitloss from bonus_hunt JOIN hunt on bonus_hunt.id = hunt.id_bonushunt GROUP BY id');
+    const stmt = db.prepare('SELECT id, title, start,date,(Select count(*) from hunt where hunt.id_bonushunt = bonus_hunt.id) as nbbonus, ((SELECT SUM(payout) FROM hunt where hunt.id_bonushunt = bonus_hunt.id)-(SELECT start FROM bonus_hunt where bonus_hunt.id = hunt.id_bonushunt)) as profitloss from bonus_hunt JOIN hunt on bonus_hunt.id = hunt.id_bonushunt GROUP BY id');
     const res = stmt.all();
     return res;
 }
