b4496c55891b0f359ae89e22fb0095d0769783e5
McSon2 <maximesaltet@gmail.com>
2022-03-19 22:14:51 +0100

    parmas

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const { dirname } = require("path");
 const path = require("path");
 const fs = require("fs");
 const { electron } = require("process");
+const { URLSearchParams } = require("url");
 const ipc = ipcMain;
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
